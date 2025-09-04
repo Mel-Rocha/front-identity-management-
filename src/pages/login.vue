227b26203991f7ccc56ee3e2b7c0b5917d7a0473
Mel-Rocha <rochamel@gmail.com>
2025-09-04 14:06:25 -0300

    feat: Versão inicial endpoint de recuperação de senha

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -12,6 +12,26 @@ const form = ref({
   remember: false,
 })
 
+const showRecoverEmail = ref(false)
+const recoverEmail = ref('')
+
+const recoverPassword = async () => {
+  if (!recoverEmail.value) {
+    error.value = 'Informe seu e-mail para recuperar a senha.'
+    return
+  }
+  try {
+    const response = await axios.post('http://0.0.0.0:8000/users/recover_password-service/', {
+      email: recoverEmail.value
+    })
+    error.value = 'Verifique seu e-mail para instruções de recuperação.'
+    showRecoverEmail.value = false
+    recoverEmail.value = ''
+  } catch (err) {
+    error.value = err.response?.data?.message || 'Erro ao recuperar senha'
+    console.error(err)
+  }
+}
 const isPasswordVisible = ref(false)
 
 const error = ref('')
@@ -62,14 +82,18 @@ const login = async () => {
               v-model="form.remember"
               label="Remember me"
             />
-            <a class="text-primary" href="javascript:void(0)">
+            <span
+              class="text-primary"
+              style="cursor:pointer"
+              @click="showRecoverEmail = true"
+            >
               Forgot Password?
-            </a>
+            </span>
           </div>
           <VBtn block type="submit">
             Login
           </VBtn>
-          <div v-if="error">{{ error }}</div>
+          <div v-if="error" class="text-error">{{ error }}</div>
         </VCol>
         <VCol cols="12" class="text-body-1 text-center">
           <span class="d-inline-block">New on our platform?</span>
@@ -87,5 +111,23 @@ const login = async () => {
         </VCol>
       </VRow>
     </VForm>
+    <VDialog v-model="showRecoverEmail" max-width="400">
+      <VCard>
+        <VCardTitle>Recuperar senha</VCardTitle>
+        <VCardText>
+          <VTextField
+            v-model="recoverEmail"
+            label="Email"
+            type="email"
+            required
+          />
+          <div v-if="error" class="text-error">{{ error }}</div>
+        </VCardText>
+        <VCardActions>
+          <VBtn color="primary" @click="recoverPassword">Enviar</VBtn>
+          <VBtn text @click="showRecoverEmail = false">Cancelar</VBtn>
+        </VCardActions>
+      </VCard>
+    </VDialog>
   </VCardText>
 </template>
