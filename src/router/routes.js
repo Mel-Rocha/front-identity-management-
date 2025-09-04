79cd073994aee95adcfea7ef21b35b74357d2c52
Mel-Rocha <rochamel@gmail.com>
2025-09-04 16:27:38 -0300

    feat: Tela de detalhes do usuário

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,12 @@
 import Login from '@/pages/login.vue'
 import Dashboard from '@/pages/dashboard.vue'
 import Register from '@/pages/register.vue'
+import Account from '@/views/pages/account-settings/AccountSettingsAccount.vue'
 
 export const routes = [
   { path: '/login', name: 'Login', component: Login },
     { path: '/register', name: 'Register', component: Register },
+    { path: '/account-settings', name: 'AccountSettings', component: Account },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   // você pode adicionar mais rotas: registro, perfil, etc.
 ]
---
+++ src/services/userService.js
@@ -0,0 +1,12 @@
+// src/services/userService.js
+import axios from 'axios'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export async function fetchMe(token) {
+  const response = await axios.get(`${API_BASE_URL}/users/me/`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  return response.data
+}
